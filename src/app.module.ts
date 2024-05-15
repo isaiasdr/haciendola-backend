7340e7d481c15dceb9b8923ee88248df425087f0
Isaias Dominguez <isaiasdr21@gmail.com>
2024-05-14 22:23:35 -0400

    Update App module and main file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ProductsModule, SeedModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('main.ts');
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix(`api/v${envs.api_version}`);
+
+  const config = new DocumentBuilder()
+    .setTitle('Haciendola Backend')
+    .setDescription('Backend for the challenge')
+    .setVersion(`${envs.api_version}`)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
